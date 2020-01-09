c4e36eca0ee5c34fbe34039d07c278a2a98166dc
Dick Kimzey <52766741+wattskimzey@users.noreply.github.com>
2020-01-09 13:34:38 -0600

    Updates

---
+++ precache-manifest.512946a2f898747248c8f06f3beeea8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f4326922c5bb02d41f3",
+    "url": "/clicky_game/static/css/main.1ed3d0f4.chunk.css"
+  },
+  {
+    "revision": "3f4326922c5bb02d41f3",
+    "url": "/clicky_game/static/js/main.4a671a84.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  },
+  {
+    "revision": "49db0d9fc976f003b3ff",
+    "url": "/clicky_game/static/js/2.458af4f8.chunk.js"
+  },
+  {
+    "revision": "fa6210c1decce131d24e9973e79531a0",
+    "url": "/clicky_game/index.html"
+  }
+];
